3ed31284dfb526ba47061599be9a14b0256920eb
jtominaga <jtominaga@unm.edu>
2018-12-12 11:41:37 -0600

    add function

---
+++ R/read 6800dir.R
@@ -0,0 +1,31 @@
+#' Reads files from the Li-Cor 6800 including Ci measured directly
+#' \code{read_6800dir} Reads Li-Cor 6800 files, which are deliminated by spaces and tabs.
+#'
+#' @param filename A character string of the form: "mydata".
+#' @param skiplines A nuber specifying the number of header lines to skip.
+#'
+#' @return read_6800 imports a Li-Cor 6800 file as a data frame
+#' @importFrom utils read.delim
+#' @export
+#'
+#'
+#'
+read_6800dir <- function(filename, skiplines){
+  skiplines <- ifelse(missing(skiplines) == TRUE, 54, skiplines)
+  # Read in file for column names ---------------------------
+  colname <- read.delim(filename, sep = "\t", skip = skiplines,
+                        header = TRUE, fill = TRUE)
+
+  # Read in file --------------------------------------------
+  data <- read.delim(filename, sep = "\t", skip = skiplines+2,
+                     header = FALSE, fill = TRUE)
+
+  # Assign column names -------------------------------------
+  colnames(data) <- colnames(colname)
+
+  data$cidifference <- data$Ci-data$CO2_B
+
+  # Print data ----------------------------------------------
+  return(data)
+}
+
