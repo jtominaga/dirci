ba883fc56242a2857d63ed78811f4ee5bf21d5e8
Joseph Ronald Stinziano <josephstinziano@gmail.com>
2018-12-13 11:59:04 -0700

    Added functions

--- R/fitcond.R
+++ R/fitcond.R
@@ -2,10 +2,7 @@
 #' \code{fitcond} Fits cuticular and intercellular conductances
 #'
 #' @param data A dataframe.
-#' @param rco2 Resistance to CO2 diffusion
-#' @param gco2 Conductance to CO2
-#' @param rsc  Stomatal resistance to CO2
-#' @param gsc  Stomatal conductance to CO2
+#' @param D    Difference in total and stomatal resistances to CO2 diffusion
 #' @param gsw  Stomatal conductance to water
 #' @param gcw  Cuticular conductance to water
 #' @param gias Intercellular conductance to water?
@@ -16,14 +13,11 @@
 #'
 #'
 #'
-fitcond <- function(data, rco2, gco2, rsc, gsc, gsw, gcw, gias){
-  rco2   <- ifelse(missing(rco2) == TRUE, 1 / gco2, rco2)
-  rsc    <- ifelse(missing(rsc)  == TRUE, 1 / gsc,  rsc)
-  data$D <- data$rco2 - data$rsc
-  m      <- nls(data$D ~ 1.6 * (1 / (data$gsw - data$gcw) - 1 / data$gsw)
-                + 1 / data$gias,
-                start = list(data$gcw = 0, data$gias = 0.1))
-  return(m)
+fitcond <- function(data, D, gsw, gcw, gias){
+  m      <- nls(D ~ 1.6 * (1 / (gsw - gcw) - 1 / gsw)
+                + 1 / gias, data = data,
+                start = list(gcw = 0, gias = 0.1))
   cond   <- coef(m)
-  return(cond)
+  output <- as.list(c(m, cond, summary(m)))
+  return(output)
 }
