e1520ec065251072627985f480a807a2257d5086
Joseph Ronald Stinziano <josephstinziano@gmail.com>
2018-12-13 14:23:31 -0700

    Added option
    Allows Harley method to be fit without measuring Ci

--- R/procharley.R
+++ R/procharley.R
@@ -9,14 +9,16 @@
 #' @param gc constant for the temperature response of gstar
 #' @param Rd dark respiration
 #' @param Tleaf specifies leaf temperature to use
+#' @param cimeas TRUE or FALSE, was Ci directly measured?
 #'
 #' @return
 #' @export
 #'
 #' @examples
-procharley <- function(data, a, b, gstar, gea, gc, Rd, Tleaf){
+procharley <- function(data, a, b, gstar, gea, gc, Rd, Tleaf, cimeas){
   a <- ifelse(missing(a) == TRUE, 0.84, a)
   b <- ifelse(missing(b) == TRUE, 0.5, b)
+  cimeas <- ifelse(missing(cimeas) == TRUE, TRUE, FALSE)
   #Need temperature response of gstar below
   gstar <- ifelse(missing(gstar) == TRUE, 37.43, gstar)
   gea <- ifelse(missing(gea) == TRUE, 24.46, gea)
@@ -27,8 +29,9 @@ procharley <- function(data, a, b, gstar, gea, gc, Rd, Tleaf){
   data$J <- a * b * data$Qin * data$PhiPS2
   data$Cc <- (data$gstar * (data$J + 8 * (data$A + data$Rd)) /
                 (data$J - 4 * (data$A + data$Rd)))
-  data$gCO2 <- data$A / (data$CO2_B - data$Cc)
-  data$gCO2_uncorrected <- data$A / (data$Ci - data$Cc)
+  data$gco2_calc <- data$A / (data$Ci - data$Cc)
+  ifelse(cimeas == TRUE, data$gco2 <- data$A / (data$CO2_B - data$Cc),
+         data$gco2 <- data$gco2_calc)
   data$rco2   <- 1 / data$gco2
   data$rsc    <- 1 / data$gsc
   data$D <- data$rco2 - data$rsc
